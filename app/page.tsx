5dd3c02936a4a1fac798eec94913418543b78c8f
Mohammad Rahil <mohammadrahil@tricog.com>
2026-06-09 11:46:00 +0530

    Fix overlap between title and tagline

--- app/page.tsx
+++ app/page.tsx
@@ -121,7 +121,7 @@ export default function Home() {
         <div className="block-group" style={{ marginBottom: 0 }}>
 
           <div style={{ display: 'inline-block', textAlign: 'left' }}>
-            <h2 className="modern-title" style={{ marginTop: '12rem', marginBottom: '-0.5rem', textAlign: 'left', fontSize: '5rem' }}>
+            <h2 className="modern-title" style={{ marginTop: '12rem', marginBottom: '0.5rem', textAlign: 'left', fontSize: '5rem' }}>
               Where <span className="accent-text">Complexity</span><br />
               <span style={{ display: 'inline-block', marginLeft: '0.6em' }}>Becomes Clarity</span>
             </h2>
